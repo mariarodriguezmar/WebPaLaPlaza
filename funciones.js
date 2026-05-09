62b69a8d38c40795dcad67f73dd2f64bdfa5c61d
maria rodriguez <lunamaria2816@gmail.com>
2026-05-08 22:59:00 -0500

    Fead: creación Parallax intro en CSS, JS y modificación de contenedor en html

--- funciones.js
+++ funciones.js
@@ -17,3 +17,25 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape') cerrarMenu();
 });
 
+
+
+/*Parallax intro*/
+window.addEventListener("scroll", parallaxScroll);
+
+function parallaxScroll(){
+
+  const scrolled = window.scrollY;
+
+  document.querySelector(".capa1").style.transform =
+  `translateY(-${scrolled * 0.08}px)`;
+
+  document.querySelector(".capa2").style.transform =
+  `translateY(-${scrolled * 0.16}px)`;
+
+  document.querySelector(".capa3").style.transform =
+  `translateY(-${scrolled * 0.28}px)`;
+
+  document.querySelector(".capa4").style.transform =
+  `translateY(-${scrolled * 0.4}px)`;
+
+}
